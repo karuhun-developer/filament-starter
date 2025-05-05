212e951bffdd89df0192ccac6de2331a6ece25ec
bayurifkialghifari <bayurifkialgh@gmail.com>
2025-05-05 13:08:56 +0700

    initial commit

---
+++ app/Filament/Resources/SliderResource.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SliderResource\Pages;
+use App\Filament\Resources\SliderResource\RelationManagers;
+use App\Models\Slider;
+use Filament\Forms;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
+
+class SliderResource extends Resource
+{
+    protected static ?string $model = Slider::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title')
+                    ->required()
+                    ->maxLength(255),
+                RichEditor::make('description')
+                    ->label('Description')
+                    ->required()
+                    ->maxLength(65535)
+                    ->disableToolbarButtons([
+                        'attachFiles',
+                        'codeBlock',
+                        'h1',
+                        'h2',
+                        'h3',
+                        'h4',
+                        'h5',
+                        'h6',
+                        'indent',
+                        'outdent',
+                        'strikeThrough',
+                        'table',
+                    ]),
+                Toggle::make('status')
+                    ->label('Status')
+                    ->default(true)
+                    ->required(),
+                SpatieMediaLibraryFileUpload::make('image')
+                    ->label('Image')
+                    ->collection('images')
+                    ->required(),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->label('Title')
+                    ->description(fn (Slider $record) => new HtmlString($record->description))
+                    ->sortable()
+                    ->searchable(),
+                ToggleColumn::make('status')
+                    ->label('Status')
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->sortable()
+                    ->dateTime()
+                    ->searchable(),
+                SpatieMediaLibraryImageColumn::make('image')
+                    ->label('Image')
+                    ->collection('images')
+                    ->conversion('thumb')
+                    ->size(50)
+                    ->circular(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSliders::route('/'),
+            'create' => Pages\CreateSlider::route('/create'),
+            'edit' => Pages\EditSlider::route('/{record}/edit'),
+        ];
+    }
+}
